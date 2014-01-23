d63f2b523b2c5da6a67b51c8d602daad99699666
Dmitriy <dmitriy_bondarev@inbox.ru>
2014-01-23 22:46:04 +0400

    Create a design in the class BDMediaPlayerController

--- AudioPlayer/AudioPlayer/BDMediaPlayerController.h
+++ AudioPlayer/AudioPlayer/BDMediaPlayerController.h
@@ -10,19 +10,33 @@
 
 
 @interface BDMediaPlayerController : UIViewController 
-{
-    NSTimer				*updateTimer;
-    UILabel				*titleLabel;
-    UILabel				*artistLabel;
-	UILabel				*albumLabel;
-	UILabel				*indexLabel;
-}
+
+@property (nonatomic, retain) NSTimer *updateTimer;
+//отображение на Tab Bar названия песни, артиста, альбома, продолжительности
+@property (nonatomic, retain) UILabel *titleLabel, *artistLabel, *albumLabel, *indexLabel, *duration, *currentTime;
+//ползунок (для продолжительности трека)
+@property (nonatomic, retain) UISlider *progressSlider;
+//Содержит ??
+@property (nonatomic, retain) UIView *containerView;
+//Клавиша чтобы при нажатии на cover альбома открывалось дополнительное окно с длиной трека и прочей информацией
+@property (nonatomic, retain) UIButton *artWorkView;
+
+//??
+@property (nonatomic, retain) UIImageView *reflectionView;
+//??
+
+@property (nonatomic, retain) CAGradientLayer *gradientLayer;
 
 //TODO (описание почему создаю еще один массив с данными)мне приходится принимать массив выбранных пользователем песен (иначе я не мог бы группировать песни по алфавиту или по определенному исполнителю - у меня бы запускался MediaPlayer с нужной песней но нажимая на клавиши назад и вперед я бы открывал не группированные песни а те которые в массиве BDSongSyorege следуют за данным треком)
 -(BDMediaPlayerController *)initWithMasSong:(NSArray *)masSong IndexSong:(int)index;
 
 //метод для возврата к списку песен
 -(void)dismissAudioPlayer;
+//метод для клавиши (artWirkView) открывает дополнительную информацию по треку
+-(void)showOverlayView;
+//без понятия что это за метод
+- (UIImage *)reflectedImage:(UIButton *)fromImage withHeight:(NSUInteger)height;
+-(void)
 
 @end
 
--- AudioPlayer/AudioPlayer/BDMediaPlayerController.m
+++ AudioPlayer/AudioPlayer/BDMediaPlayerController.m
@@ -28,6 +28,16 @@
 
 @implementation BDMediaPlayerController
 
+static const CGFloat kDefaultReflectionFraction = 0.65;
+static const CGFloat kDefaultReflectionOpacity = 0.40;
+
+@synthesize updateTimer, titleLabel, artistLabel, albumLabel, indexLabel, duration, currentTime;
+@synthesize progressSlider;
+@synthesize containerView;
+@synthesize artWorkView;
+@synthesize reflectionView;
+@synthesize gradientLayer;
+
 
 - (void)viewDidLoad
 {
@@ -86,6 +96,44 @@
     albumLabel.textAlignment = NSTextAlignmentCenter;
     albumLabel.lineBreakMode = NSLineBreakByTruncatingTail;
     [self.view addSubview:albumLabel];
+    
+    //TODO уточнить (самому)
+    duration.adjustsFontSizeToFitWidth = YES;
+    currentTime.adjustsFontSizeToFitWidth = YES;
+    progressSlider.minimumValue = 0.0;
+    
+    //TODO разобраться для чего контейнер??
+    containerView = [[UIView alloc]initWithFrame:CGRectMake(0, 44, self.view.bounds.size.width, self.view.bounds.size.height - 44) ];
+    [self. view addSubview:containerView];
+    
+    //при нажатии на клавишу открывается дополнительное окно (информация по треку) - клавиша размером с cover альбома
+    artWorkView = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 320, 320)];
+    //для кнопки использовать катринку cover альбома
+    [artWorkView setImage:[selectedSong getCoverImage] forState:UIControlStateNormal];
+    [artWorkView addTarget:self action:@selector(showOverlayView) forControlEvents:UIControlEventTouchUpInside];
+    //TODO уточнить (самому) - это настройки клавиши для отображения доп инфы по треку
+    artWorkView.showsTouchWhenHighlighted = NO;
+    artWorkView.adjustsImageWhenHighlighted = NO;
+    artWorkView.backgroundColor = [UIColor clearColor];
+    [containerView addSubview:artWorkView];
+    
+    //TODO уточнить что (сам)
+    reflectionView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 320, 320, 96)];
+    reflectionView.image = [self reflectedImage:artWorkView withHeight:artWorkView.bounds.size.height * kDefaultReflectionFraction];
+    reflectionView.alpha = kDefaultReflectionFraction;
+    [self.containerView addSubview:reflectionView];
+    
+    gradientLayer = [[CAGradientLayer alloc] init];
+    gradientLayer.frame = CGRectMake(0.0 , self.containerView.bounds.size.height - 96,  self.containerView.bounds.size.width, 48);
+    gradientLayer.colors = [NSArray arrayWithObjects:(id)[UIColor clearColor].CGColor, (id)[UIColor colorWithWhite:0.0 alpha:0.5].CGColor, (id)[UIColor blackColor]. CGColor, (id)[UIColor blackColor].CGColor, nil];
+    gradientLayer.zPosition = INT_MAX;
+    
+    //Панель управления исполнением (play и так далее)
+    
+    UIView *v = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 10) ];
+    v.backgroundColor = [UIColor clearColor];
+    
+
     
     
 }
